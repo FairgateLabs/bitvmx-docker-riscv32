542ba5a7cfc04a7d80fdfba88e3b80298ebc813e
crivasr <camilorivasramunni@gmail.com>
2025-08-14 17:07:43 -0300

    add division tests

--- audit.bat
+++ audit.bat
@@ -1,1 +1,2 @@
 call docker-run.bat riscv32 riscv32/build.sh src/audit/audit_01.c --output audit --with-mul
+call docker-run.bat riscv32 riscv32/build.sh src/audit/audit_02.c --output audit --with-mul

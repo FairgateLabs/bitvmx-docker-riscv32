062b05892b9b7bdb8eda30721ef5576b21a25d36
crivasr <camilorivasramunni@gmail.com>
2025-09-15 14:13:40 -0300

    rename tests

--- audit.bat
+++ audit.bat
@@ -1,4 +1,4 @@
 call docker-run.bat riscv32 riscv32/build.sh src/audit/audit_01.c --output audit --with-mul
 call docker-run.bat riscv32 riscv32/build.sh src/audit/audit_09_verify.c --output audit --with-mul
-call docker-run.bat riscv32 riscv32/build.sh src/audit/audit_12.c --output audit --with-mul
-call docker-run.bat riscv32 riscv32/build.sh src/audit/audit_13.c --output audit --with-mul
+call docker-run.bat riscv32 riscv32/build.sh src/audit/audit_12_verify.c --output audit --with-mul
+call docker-run.bat riscv32 riscv32/build.sh src/audit/audit_13_verify.c --output audit --with-mul
